0abe2f57abe3b62f0e7d4329360d15b018f26996
yovkj <2993639199@qq.com>
2025-03-17 15:53:10 +0800

    Update response.py

--- app/datatype/response.py
+++ app/datatype/response.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, Field
 from .models import User
+from typing import Optional
+
+#注册
 class UserRegisterRequest(BaseModel):
     username: str = Field(..., min_length=5, max_length=16, description="用户名")
     password: str = Field(..., min_length=5, max_length=16, description="密码")
@@ -11,7 +14,7 @@ class CommonResponse(BaseModel):
 
     def dict(self, **kwargs):
         return super().dict(exclude_unset=True, **kwargs)
-
+#登录
 class LoginRequest(BaseModel):
     username: str = Field(..., min_length=5, max_length=16, description="用户名")
     password: str = Field(..., min_length=5, max_length=16, description="密码")
@@ -20,3 +23,9 @@ class LoginResponse(BaseModel):
     code: int
     message: str | None = None
     data: str | None = Field(None, description="JWT令牌")
+
+# 获取用户详细信息
+class UserInfoResponse(BaseModel):
+    code: int
+    message: Optional[str] = None
+    data: Optional[dict] = Field(None, description="用户详细信息")
