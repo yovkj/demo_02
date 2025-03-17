de0d4cd9b4fb75a10fbde9aac26e9b3cf7f34a60
yovkj <2993639199@qq.com>
2025-03-17 15:53:19 +0800

    Update __init__.py

--- app/router/__init__.py
+++ app/router/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.api.v1.user_register import router as user_register_router
 from app.api.v1.login import router as login_router  # 新增登录路由导入
-def register_routers(app: FastAPI):
-    app.include_router(user_register_router, prefix="/api")
+from app.api.v1.user_info import router as user_info_router  # 新增获取路由导入
 
+
+#获取用户详情
 def register_routers(app: FastAPI):
     app.include_router(user_register_router, prefix="/api")
-    app.include_router(login_router, prefix="/api")  # 注册登录路由
+    app.include_router(login_router, prefix="/api")
+    app.include_router(user_info_router, prefix="/api")  # 注册新路由
