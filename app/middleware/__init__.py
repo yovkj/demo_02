5d6f1fba744605912692bba25dc40acdcc583434
yovkj <2993639199@qq.com>
2025-03-17 15:53:26 +0800

    Update __init__.py

--- app/middleware/__init__.py
+++ app/middleware/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request, HTTPException
 
 
 def register_middlewares(app: FastAPI):
@@ -13,5 +14,14 @@ def register_middlewares(app: FastAPI):
     )
 
 
+async def token_verification_middleware(request: Request, call_next):
+    # 从请求头获取令牌
+    token = request.headers.get("Authorization")
+    if token:
+        token = token.replace("Bearer ", "")  # 去除 Bearer 前缀
+    if not token:
+        raise HTTPException(status_code=401, detail="令牌缺失")
+    response = await call_next(request)
+    return response
 
 
