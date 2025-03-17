03a9c0798f98daa26bb48b6a27e51c17d19f0aa1
yovkj <2993639199@qq.com>
2025-03-17 15:53:08 +0800

    Update __init__.py

--- app/initializer/conf/__init__.py
+++ app/initializer/conf/__init__.py
@@ -1,6 +1,4 @@
-import importlib
 from config import settings
-
 
 class Config:
     def __init__(self):
@@ -8,6 +6,7 @@ class Config:
         self.appversion = settings.APP_VERSION
         self.debug = settings.DEBUG
         self.db_url = settings.DB_URL
-
+        self.SECRET_KEY = settings.SECRET_KEY
+        self.ALGORITHM = settings.ALGORITHM
 
 g = Config()
